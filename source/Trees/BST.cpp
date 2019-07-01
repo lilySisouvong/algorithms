f6a8d1d8cd684a42ce20a8197be045b427155eac
Lily Sisouvong <lilysisouvong@Lilys-MacBook-Air.local>
2019-07-01 17:51:02 -0400

    hi

--- source/Trees/BST.cpp
+++ source/Trees/BST.cpp
@@ -1,99 +1,5 @@
 #include "BST.hpp"
 
-<<<<<<< HEAD
-BST::BST(){
-    root = nullptr;
-}
-BST::~BST(){
-    return;
-}
-
-/** insert(int data)
-   * Inserts the given data into the tree.
-   * Does nothing if the data is already in the tree.
-   */
-void BST::insert(int data){
-    Node* p = root;
-    Node* ins = new Node(data);
-    if(search(data)) throw "Already in tree!";
-    if (is_empty()){
-        root = ins;
-        ins->left = nullptr;
-        ins->right = nullptr;
-    }
-    while(p!=nullptr){
-        if(data>p->data) p=p->right;
-        else p = p->left;
-    }
-    if(data>p->data){
-        p->right = ins;
-        ins->right = nullptr;
-        ins->left = nullptr;  
-    }
-    else{
-        p->left = ins;
-        ins->right = nullptr;
-        ins->left = nullptr;   
-    }
-}
-
-/** is_empty()
-   * Returns true if the tree is empty and false otherwise
-   */
-bool BST::is_empty(){
-    return (root == nullptr);
-}
-
-/** search(int data)
-   * Searches the tree for the given data, returning true if the data
-   * is found and false otherwise.
-   */
-bool BST::search(int data){
-    Node* p = root;
-    return(root->data==data);
-    while(p!=nullptr){
-        if(data>p->data) p= p->right;
-        else if(data == p->data) return true;
-        else p = p->left;
-    }
-    return 0;
-}
-
-/** height()
-   * Determines and returns the height of the tree.
-   * Returns -1 if the tree is empty.
-   */
-int max(int a, int b){
-    return(a>b)? a: b;
-}
-
-int BST::height(Node* n){
-    return (!n) ? -1:1+ max(height(n->left), height(n->right));
-}
-
-int BST::height(){
-    return height(root);
-}
-
-void BST::remove(int data){
-
-}
-
-void BST::clear(){
-
-}
-void BST::preorder(std::ostream& oss){
-
-}
-
-void BST::inorder(std::ostream& oss){
-
-}
-
-void BST::postorder(std::ostream& oss){
-
-}
-=======
 BST::BST() {
   root = nullptr;
 }
@@ -119,6 +25,7 @@ bool BST::is_empty() {
   return !root;
 }
 
+
 bool BST::search(int data, Node* n) {
   if (!n)
     return false;
@@ -134,9 +41,51 @@ bool BST::search(int data) {
   return search(data, root);
 }
 
-Node* BST::remove(int data, Node* n) {}
+Node* BST::remove(int data, Node* n) {
+  Node* p= n; 
+  if (height(p)==0){
+    delete p;
+    //set parent ptr to Null
+  }
+  else if((p->left && !p->right)){
+    if(!p->left->left){
+      p->data= p->left->data;
+      p->left = nullptr;
+    }
+    else{
 
-void BST::remove(int data) {}
+    }
+  } 
+  else if ((p->right && !p->left)){
+
+  }
+  else if(){
+
+  }
+  else if (search(data,n)){
+    while(p){
+      if (p->data < data){
+        p = p->right;
+      }
+      else if (p->data == data){
+        delete p;
+      }
+      else{
+        p = p->left;
+      }
+    }
+  }
+  else{
+    throw "Not in tree!";
+    }
+  }
+
+
+
+void BST::remove(int data) {
+  Node* p= root; 
+  remove(data,p);
+}
 
 int max(int a, int b) {
   return (a > b) ? a : b;
@@ -156,15 +105,33 @@ void BST::clear(Node* n) {}
 
 void BST::clear() {}
 
-void BST::preorder(Node* n, std::ostream& oss) {}
+void BST::preorder(Node* n, std::ostream& oss) {
+  Node* p = n;
+  if(!n) return;
+  oss<<n<<", ";
+  preorder(n->left, oss);
+  preorder(n->right, oss);
+}
 
-void BST::preorder(std::ostream& oss) {}
+void BST::preorder(std::ostream& oss){
+  Node* p = root;
+  preorder(p, oss);
+  oss<<p<<", ";
+}
 
-void BST::inorder(Node* n, std::ostream& oss) {}
+void BST::inorder(Node* n, std::ostream& oss) {
+  Node* p = n;
+  if(!n) return;
+  oss<<n<<", ";
+  inorder(n->left, oss);
+  inorder(n->right, oss);
+}
 
-void BST::inorder(std::ostream& oss) {}
+void BST::inorder(std::ostream& oss) {
+    Node* p = root;
+    
+}
 
 void BST::postorder(Node* n, std::ostream& oss) {}
 
 void BST::postorder(std::ostream& oss) {}
->>>>>>> upstream/master
