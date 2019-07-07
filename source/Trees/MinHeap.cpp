35df07f9ca932bcbda58c6038245a2bea4a49494
Lily Sisouvong <lilysisouvong@Lilys-MacBook-Air.local>
2019-07-07 18:49:26 -0400

    bro

--- source/Trees/MinHeap.cpp
+++ source/Trees/MinHeap.cpp
@@ -1,92 +1,5 @@
 #include "MinHeap.hpp"
 
-<<<<<<< HEAD
-MinHeap::MinHeap(unsigned int cap){
-    array = new int[cap];
-    capacity = cap-1;
-    size = 0;
-}
-
-MinHeap::~MinHeap(){
-    delete[] array;
-}
-
-
-void MinHeap::push(int data){
-    if(size == capacity){
-        int array2[capacity*2];
-        for(int i = 1; i < size; ++i){
-            array2[i] = array[i];
-        }
-        capacity *= 2;
-        delete[] array;
-        array = array2;
-        }
-    array[size] = data;
-    size++;
-    swim();
-}
-
-int MinHeap::count(){ 
-    return size;
-}
-
-int MinHeap::pop(){
-    return 0;
-}
-
-int MinHeap::peek(){
-    if (size == 0){
-        throw "Nothing there!";
-    }
-    return array[1];
-}
-
-bool MinHeap::search(int data){
-    for(int i = 1; i < size; ++i){
-        if(data == array[i]){
-            return true;
-        }
-    }
-    return false;
-}
-
-int MinHeap::remove(int i){
-    return 0;
-}
-
-void MinHeap::erase(int data){
-
-}
-
-void MinHeap::print(std::ostream &oss){
-}
-
-void swap(int* y, int* x) {
-  int temp;
-  temp = *y;
-  *y = *x;
-  *x = temp;
-}
-
-void MinHeap::swim(){ 
-    int temp = size;
-    while(array[temp] < array[temp/2] && temp !=1){
-        swap(&array[temp], &array[temp/2]);
-        temp = temp / 2;
-    }
-}
-void sink(int i){
-
-}
-
-bool search(int i, int data){
-    return true;
-}
-
-void erase(int i, int data){
-
-=======
 inline void swap(int* a, int* b) {
   int temp = *a;
   *a = *b;
@@ -200,5 +113,4 @@ void MinHeap::print(std::ostream& oss) {
     oss << array[i] << ", ";
   }
   oss << '\n';
->>>>>>> upstream/master
 }
