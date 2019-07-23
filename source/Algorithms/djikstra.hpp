c0a4b29bfe4a3dba52fc1da03bab4a260d623329
Lily Sisouvong <lilysisouvong@176.191.20.172.s.wireless.uri.edu>
2019-07-23 16:53:51 -0400

    done

--- source/Algorithms/djikstra.hpp
+++ source/Algorithms/djikstra.hpp
@@ -8,5 +8,27 @@
 
 #include "../Graphs/Graph.hpp"
 #include "../String/String.hpp"
+#include "../Queue/Queue.hpp"
+#include "../Functions/functions.hpp"
 
-Graph djikstras(Graph& G, int vertex);
+#define UNVISITED 0
+#define VISITED 1
+
+Graph djikstras(Graph& G, int vertex){
+    std::vector<int> neighbors = G.neighbors(vertex);
+    int arr[neighbors.size()];
+    Queue queue;
+    int minimum= vertex.weight;
+    G.set_value(vertex,VISITED);
+    for(int i = 0; i<neighbors.size(); ++i){
+        G.set_value(neighbors[i], VISITED);
+        queue.push(neighbors[i]);
+        
+    }
+    for(int j = 0 ; j < neighbors.size(); ++j){
+        minimum = G.min(minimum, neighbors[j].weight);
+        
+    }
+
+    return dijikstras(G, minimum);
+}
